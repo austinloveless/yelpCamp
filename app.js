1a1457a375d9ca4639a79654f1566f65d322a791
austinloveless <austinloveless5171@gmail.com>
2018-07-13 14:01:22 -0600

    added campgrounds, post route, and form

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+  {
+    name: "Salmon Creek",
+    image:
+      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
+  },
+  {
+    name: "Mountain Goats rest",
+    image:
+      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
+  }
+];
 
 app.get("/", (req, res) => {
   res.render("landing");
+});
+
+app.get("/campgrounds", (req, res) => {
+  res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", (req, res) => {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("new");
 });
 
 app.listen(port, () => {
