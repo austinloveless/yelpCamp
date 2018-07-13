0dfca13233075972e226697f5c63ef4dfc0e9cea
austinloveless <austinloveless5171@gmail.com>
2018-07-13 13:08:40 -0600

    just making a repo

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("landing");
+});
+
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
