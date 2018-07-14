f9690d662242fad731c642fcc9c6f5e6a2db1db5
austinloveless <austinloveless5171@gmail.com>
2018-07-13 21:18:45 -0600

    added mongodb, show page, and more RESTful routes

--- app.js
+++ app.js
@@ -2,71 +2,77 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
-  },
-  {
-    name: "Mountain Goats rest",
-    image:
-      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
-  },
-  {
-    name: "Mountain Goats rest",
-    image:
-      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg"
-  }
-];
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Granite Hill",
+//     image:
+//       "https://res.cloudinary.com/simpleview/image/upload/c_limit,f_auto,h_1200,q_75,w_1200/v1/clients/poconos/Campgrounds_Tent_Sites_Woman_Hemlock_Campground_4_PoconoMtns_06f196d5-8814-4803-a132-8a4daae1755e.jpg",
+//     description:
+//       "This is a huge granite hill, no bathrooms, No water. Beautiful granite!"
+//   },
+//   (err, campground) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("new campground:", campground);
+//     }
+//   }
+// );
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { campgrounds: allCampgrounds });
+    }
+  });
 });
 
 app.post("/campgrounds", (req, res) => {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  var desc = req.body.description;
+  var newCampground = { name: name, image: image, description: desc };
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get("/campgrounds/new", (req, res) => {
   res.render("new");
+});
+
+app.get("/campgrounds/:id", (req, res) => {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", { campground: foundCampground });
+    }
+  });
 });
 
 app.listen(port, () => {
